b875136023e77b0be9954418f9eb54f78526ac9d
rungene <rungenesamuel@gmail.com>
2021-03-14 16:23:27 +0300

    Override onSizeChanged() inside MyCanvasView.kt

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -1,8 +1,49 @@
 package com.example.android.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context):View(context) {
+
+    //define member variables for a canvas and a bitmap
+    //These are your bitmap and canvas for caching what has been drawn before.
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+   /* define a class level variable backgroundColor, for the background color of the canvas and
+    initialize it to the colorBackground you defined earlier.*/
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground,
+            null)
+
+   /* This callback method is called by the Android system with the changed screen dimensions,
+    that is, with a new width
+    and height (to change to) and the old width and height (to change from).*/
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+       /* Looking at onSizeChanged(), a new bitmap and canvas are created every time the function
+        executes. You need a new bitmap, because the size has changed. However, this is a memory
+        leak, leaving the old bitmaps around. To fix this, recycle extraBitmap before creating
+        the next one*/
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        /*create an instance of Bitmap with the new width and height, which are the screen size,
+        and assign it to extraBitmap. The third argument is the bitmap color configuration.
+        ARGB_8888 stores each color in 4 bytes and is recommended.*/
+
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+       // Create a Canvas instance from extraBitmap and assign it to extraCanvas.
+        extraCanvas = Canvas(extraBitmap)
+        //Specify the background color in which to fill extraCanvas.
+        extraCanvas.drawColor(backgroundColor)
+
+
+    }
 
 }
